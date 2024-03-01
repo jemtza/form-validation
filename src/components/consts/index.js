ebc7ca8bd787dbdb1c5fde2e89f9af840959b339
Jorge Martinez <jmartineza0809@gmail.com>
2024-03-01 00:05:58 -0800

    wip: create default and options

---
+++ src/components/consts/index.js
@@ -0,0 +1,23 @@
+export const DEFAULT_FORM_DATA = {
+  username: "",
+  role: "",
+  email: "",
+  hobbies: [],
+};
+
+export const roles = [
+  { id: 0, name: "Admin" },
+  { id: 1, name: "User" },
+];
+
+export const hobbies = [
+  { id: 1, value: "Reading" },
+  { id: 2, value: "Coding" },
+  { id: 3, value: "Drawing" },
+  { id: 4, value: "Watching Movies" },
+  { id: 5, value: "Listening to Music" },
+  { id: 6, value: "Playing Games" },
+  { id: 7, value: "Traveling" },
+  { id: 8, value: "Sports" },
+  { id: 9, value: "Cooking" },
+];
